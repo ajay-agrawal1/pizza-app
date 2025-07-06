86dca2f9677129ca65c7381dbdf12a564ff6ef92
Ajay Agrawal <ajayagrawal04gmail.com>
2025-07-06 10:35:38 +0530

    Update response text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🍕 Pizza order received!');
+  res.send('🍕 Your pizza is on the way!');
 });
 
 const PORT = process.env.PORT || 3000;
